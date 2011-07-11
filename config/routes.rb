abed6927af4179d17e1e8c70e9d700937d369e04
Tomasz Skiba <tomasz.r.skiba@accenture.com>
2011-07-11 17:14:49 -0400

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+
+  resources :users
 
   match '/signup',  :to => 'users#new'
+
+  #get "users/new"
+
+  #match '/signup',  :to => 'users#new'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
