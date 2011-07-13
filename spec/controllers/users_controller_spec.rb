1d0c5cddf898fcc19b44d5bb275ae5092c11319e
Tomasz Skiba <tomasz.r.skiba@accenture.com>
2011-07-13 13:13:30 -0400

    created help page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -45,6 +45,11 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
     end
+
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
   end
 
   describe "POST 'create'" do
@@ -94,6 +99,11 @@ describe UsersController do
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
+      end
+
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
       end
 
     end
